a96b2f973f51287ec9c7480bb2f0d711eb223178
saiful-islam265 <saiful@industrydive.com>
2023-11-07 10:31:21 +0600

    Update due date

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 26, 2023 15:37:25").getTime();
+var countDownDate = new Date("Dec 16, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function () {
